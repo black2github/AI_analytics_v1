4dc5c897aeed394c971c5510262a59015f4f8ada
Alexey Sen <alexey.sen@gmail.com>
2025-11-12 22:12:59 +0300

    Версия 0.27

--- app/config.py
+++ app/config.py
@@ -30,7 +30,7 @@ JIRA_API_TOKEN = os.getenv("JIRA_API_TOKEN")  # Альтернатива пар�
 LLM_PROVIDER = os.getenv("LLM_PROVIDER")
 LLM_MODEL = os.getenv("LLM_MODEL", "gpt-4") # gpt-3.5-turbo, gpt-3.5-turbo-16k, gpt-4-32k...
 LLM_TEMPERATURE = os.getenv("LLM_TEMPERATURE", "0.2")
-LLM_CONTEXT_SIZE = 65000
+LLM_CONTEXT_SIZE = 128000
 
 # openai | huggingface
 EMBEDDING_PROVIDER = os.getenv("EMBEDDING_PROVIDER", "huggingface")
